f6d912a3a2d04320ddaf94f3488530ee0135374f
Jrz5220 <felixlazo94@gmail.com>
2024-07-17 21:23:45 -0400

    Add error handling

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,5 +1,4 @@
-import requests
-import json
+import requests, json
 
 def emotion_detector(text_to_analyze):
     url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
@@ -7,13 +6,23 @@ def emotion_detector(text_to_analyze):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json = myobj, headers = header)
     formatted_response = json.loads(response.text)
-    emotions = {
-        'anger': formatted_response["emotionPredictions"][0]["emotion"]["anger"],
-        'disgust': formatted_response["emotionPredictions"][0]["emotion"]["disgust"],
-        'fear': formatted_response["emotionPredictions"][0]["emotion"]["fear"],
-        'joy': formatted_response["emotionPredictions"][0]["emotion"]["joy"],
-        'sadness': formatted_response["emotionPredictions"][0]["emotion"]["sadness"]
-    }
-    dominant_emotion = max(emotions, key = lambda k: emotions[k])
-    emotions["dominant_emotion"] = dominant_emotion
+    if response.status_code == 200:
+        emotions = {
+            'anger': formatted_response["emotionPredictions"][0]["emotion"]["anger"],
+            'disgust': formatted_response["emotionPredictions"][0]["emotion"]["disgust"],
+            'fear': formatted_response["emotionPredictions"][0]["emotion"]["fear"],
+            'joy': formatted_response["emotionPredictions"][0]["emotion"]["joy"],
+            'sadness': formatted_response["emotionPredictions"][0]["emotion"]["sadness"]
+        }
+        dominant_emotion = max(emotions, key = lambda k: emotions[k])
+        emotions["dominant_emotion"] = dominant_emotion
+    elif response.status_code == 400:
+        emotions = {
+            "anger": None,
+            "disgust": None,
+            "fear": None,
+            "joy": None,
+            "sadness": None,
+            "dominant_emotion": None
+        }
     return emotions

8b69b06c45daf271ae81739e167bfb1c228f8f89
Jrz5220 <felixlazo94@gmail.com>
2024-07-17 21:41:25 -0400

    Add docstrings

--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+''' Executing this function initiates the application of emotional
+    detector to be executed over the Flask channel and deployed on
+    localhost:5000.
+'''
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,12 +9,19 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def em_detector():
+    ''' This code receives the text from the HTML interface and 
+        runs emotional detection over it using emotional_detector()
+        function. The output returned shows each emotion's score.
+    '''
     text_to_analyze = request.args.get("textToAnalyze")
     response = emotion_detector(text_to_analyze)
     return f"For the given statement, the system response is 'anger': {response['anger']}, 'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']}, 'sadness': {response['sadness']}. The dominant emotion is {response['dominant_emotion']}"
 
 @app.route("/")
 def render_index_page():
+    ''' This function initiates the rendering of the main application
+        page over the Flask channel
+    '''
     return render_template("index.html")
 
 if __name__ == "__main__":
